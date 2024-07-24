216d5db05c6e8d2fbd5946f9c64d228b218df174
MazalMahari <mazalmahari@gmail.com>
2024-07-24 12:54:42 +0300

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 //src/api.js
 
 import axios from 'axios';
-const API_BASE_URL= 'https://my-api-app-9.onrender.com'
+const API_BASE_URL= 'https://my-api-app-19.onrender.com'
 export const getCodeBlocks = async () => {
   const response = await axios.get(`${API_BASE_URL}/api/codeblocks`);
   return response.data;
